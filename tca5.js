a36f0f793d3e6e98ff62a1a5d72a113fcade50d5
thirdcoder <boris3rd@bk.ru>
2015-07-24 18:43:58 -0700

    Move everything to worker; disable video/audio hardware for now since document and Audio unavailable

--- tca5.js
+++ tca5.js
@@ -6,25 +6,8 @@ const worker = webworkify(require('./worker.js'));
 
 worker.addEventListener('message', (ev) => {
   console.log('got worker event',ev);
-  debugger;
 });
 
-worker.postMessage(4);
+worker.postMessage('boot');
 
-const CPU = require('cpu3502');
 
-const installVideoHardware = require('./video.js');
-const installAudioHardware = require('./audio.js');
-const installTimerHardware = require('./timer.js');
-
-const cpu = CPU();
-
-installVideoHardware(cpu);
-installAudioHardware(cpu);
-installTimerHardware(cpu);
-
-global.cpu = cpu;
-
-const fs = require('fs');
-cpu.assemble_bootcode(fs.readFileSync('os.asm', 'utf8'));
-cpu.boot();
--- worker.js
+++ worker.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const CPU = require('cpu3502');
+
+const installVideoHardware = require('./video.js');
+const installAudioHardware = require('./audio.js');
+const installTimerHardware = require('./timer.js');
+
 module.exports = (self) => {
+
   self.addEventListener('message', (ev) => {
     console.log('worker got',ev.data);
-    self.postMessage('reply2');
+
+    if (ev.data === 'boot') {
+      const cpu = CPU();
+      global.cpu = cpu;
+      console.log('cpu=',cpu);
+
+      //installVideoHardware(cpu); // TODO
+      //installAudioHardware(cpu); // TODO
+      installTimerHardware(cpu);
+
+      const fs = require('fs');
+      cpu.assemble_bootcode(fs.readFileSync('os.asm', 'utf8'));
+      cpu.boot();
+      self.postMessage('booted');
+    }
   });
 };
