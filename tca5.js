7f6216c5b4ec5f6f22f99ac1b3437739e91f0b6f
thirdcoder <boris3rd@bk.ru>
2015-07-24 18:28:06 -0700

    Add test webworker

--- tca5.js
+++ tca5.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const webworkify = require('webworkify');
+
+const worker = webworkify(require('./worker.js'));
+
+worker.addEventListener('message', (ev) => {
+  console.log('got worker event',ev);
+  debugger;
+});
+
+worker.postMessage(4);
+
 const CPU = require('cpu3502');
 
 const installVideoHardware = require('./video.js');
