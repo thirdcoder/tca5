addd6f49578ce5ec20518a33992e64e95091fd4e
thirdcoder <boris3rd@bk.ru>
2015-07-25 20:58:50 -0700

    Add write command, writes data in line buffer to floppy

--- os.asm
+++ os.asm
@@ -186,6 +186,9 @@ BEQ command_help
 LDA #'r
 CMP line_buffer,Y
 BEQ command_read
+LDA #'w
+CMP line_buffer,Y
+BEQ command_write
 JMP command_bad
 
 handle_enter_done:
@@ -235,6 +238,8 @@ JMP handle_enter_done
 
 command_read:
 JMP command_read2       ; too far
+command_write:
+JMP command_write2
 
 
 .equ 45 col_count
@@ -327,9 +332,6 @@ _print_string_param:
 
 
 
-
-; read data from floppy
-command_read2:
 .equ -3290 floppy_data_ptr
 .equ -3292 floppy_name_ptr
 .equ -3294 floppy_length_ptr
@@ -337,6 +339,26 @@ command_read2:
 .equ -1 floppy_command_read
 .equ 0 floppy_command_write
 
+; write data to floppy (similar to echo text > filename TODO)
+command_write2:
+LDA #<line_buffer
+LDX #>line_buffer
+STA floppy_data_ptr     ; TODO: increment pointer to remove command prefix
+STX floppy_data_ptr+1
+
+LDA #<filename
+LDX #>filename
+STA floppy_name_ptr
+STX floppy_name_ptr+1
+
+LDA #floppy_command_write
+STA floppy_command_execute  ; TODO: print out number of trytes written? to filename?
+
+JMP handle_enter_done
+
+
+; read data from floppy TODO: rename 'cat'...? Unix
+command_read2:
 LDA #<line_buffer
 LDX #>line_buffer
 STA floppy_data_ptr
@@ -355,8 +377,6 @@ LDX #>line_buffer
 JSR print_string
 INC row
 STZ col
-
-;DNOP A
 
 JMP handle_enter_done
 
